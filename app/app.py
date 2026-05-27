32dcab6a6679bf2080219f29fe7b30a56fbf4b3b
Mansi Wani <99387887+MansiWani9@users.noreply.github.com>
2026-05-27 17:08:04 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,40 +1,74 @@
-from flask import Flask, jsonify, request
-import jwt
-import datetime
+from flask import Flask, jsonify
+from datetime import datetime
+import platform
+import socket
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'devopskey'
+@app.route('/')
+def home():
+
+    hostname = socket.gethostname()
+    os_name = platform.system()
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    html = f"""
+    <html>
+    <head>
+        <title>Lightweight DevOps Project</title>
+
+        <style>
+            body {{
+                background-color: #0f172a;
+                color: white;
+                font-family: Arial;
+                text-align: center;
+                padding-top: 100px;
+            }}
+
+            .container {{
+                background: #1e293b;
+                width: 60%;
+                margin: auto;
+                padding: 40px;
+                border-radius: 15px;
+            }}
+
+            h1 {{
+                color: #38bdf8;
+            }}
+        </style>
+    </head>
+
+    <body>
+
+        <div class="container">
+
+            <h1>🚀 Lightweight DevOps Project</h1>
+
+            <p>Application Running Successfully ✅</p>
+
+            <p><b>Hostname:</b> {hostname}</p>
+
+            <p><b>Operating System:</b> {os_name}</p>
+
+            <p><b>Current Time:</b> {current_time}</p>
+
+        </div>
+
+    </body>
+    </html>
+    """
+
+    return html
 
 
-@app.route('/login', methods=['POST'])
-def login():
-
-    token = jwt.encode(
-        {
-            'user': 'admin',
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-        },
-        app.config['SECRET_KEY'],
-        algorithm='HS256'
-    )
-
-    with open("/logs/access.log", "a") as f:
-        f.write("TOKEN GENERATED\n")
-
-    return jsonify({'token': token})
+@app.route('/health')
+def health():
+    return jsonify({
+        "status": "running"
+    })
 
 
-@app.route('/secure', methods=['GET'])
-def secure():
-
-    auth = request.headers.get('Authorization')
-
-    with open("/logs/access.log", "a") as f:
-        f.write("SECURE API HIT\n")
-
-    return jsonify({'message': 'Secure API Accessed'})
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
